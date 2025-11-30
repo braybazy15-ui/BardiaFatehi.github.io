dbeab1abc4d9e84e0f0e119a9b2a7e918f2e210e
braybazy15-ui <braybazy15@gmail.com>
2025-11-30 14:55:18 +0330

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-@app.before_request
-def before_request():
-    if not request.is_secure:
-        url = request.url.replace('http://', 'https://', 1)
-        return redirect(url, code=301)
-
 @app.route('/')
 def MainPage():
     return render_template ('MainPage.html')
@@ -52,3 +45,4 @@ def Poshtibani():
 if __name__ == '__main__':
 
     app.run(debug=True,host="0.0.0.0")
+
